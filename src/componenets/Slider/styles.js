da4ca593dba9454639fac239cdd41c9985f233a3
Michaela Stefkova <37817290+michaelastefkova@users.noreply.github.com>
2021-12-27 13:40:11 +0000

    Create styles.js

---
+++ src/componenets/Slider/styles.js
@@ -0,0 +1,11 @@
+   import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  position: relative;
+  margin-top: 68px;
+  @media (min-width: 996px) {
+    margin-top: 0;
+    max-width: 445px;
+  }
+`;
